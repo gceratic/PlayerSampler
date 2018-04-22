5b0d97e9d52fa0490fa67c232ec90eda2ac11753
gceratic <gceratic@gmail.com>
2018-04-22 12:58:20 -0500

    Add final structure

---
+++ scripts/p2.js
@@ -0,0 +1,12 @@
+var playerParam = {
+  'pcode':'Jvd2wyOrkZVDS3BJWpnvUwEidYIO',
+  'playerBrandingId':'f1f3a6490d6f480b91d0d152a863d3c5',
+  'autoplay':true,
+  'loop':true,
+  'skin': {
+          'config': '../scripts/p2.json'
+        }
+
+};
+OO.ready(function() {window.pp = OO.Player.create('container_1524368775', 'V3MDM0ZjE6VFHuScVibWmSCXWU37sTY4', playerParam);
+});
