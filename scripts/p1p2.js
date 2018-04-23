6adffacf1fbad7720aba3e2200a797bca0aab58f
gceratic <gceratic@gmail.com>
2018-04-22 20:02:56 -0500

    Add Thumbnail

--- scripts/p1p2.js
+++ scripts/p1p2.js
@@ -1,16 +1,12 @@
-var playerParam = {
-  'pcode':'Jvd2wyOrkZVDS3BJWpnvUwEidYIO',
-  'playerBrandingId':'f1f3a6490d6f480b91d0d152a863d3c5',
-  'playlistsPlugin': {'data':['5f048115febc49ec9056375198fe2e6e']},
-  'useFirstVideoFromPlaylist': true,
-  'autoplay':false,
-  'loop':false,
-  'skin': {
-          'config': '../scripts/p1p2.json'
-        }
-};
 
-
-  OO.ready(function() {
-  var pp1 = OO.Player.create('container_1524368774', "", playerParam);
-   });
+     OO.ready(function() {
+       var playerParam = {
+         "pcode": "Jvd2wyOrkZVDS3BJWpnvUwEidYIO",
+         "playerBrandingId": "f1f3a6490d6f480b91d0d152a863d3c5",
+         "autoplay": false,
+         "loop": false,
+         "playlistsPlugin": {"data":["5f048115febc49ec9056375198fe2e6e"]},
+         "useFirstVideoFromPlaylist": true
+       };
+       window.ooPlayer = OO.Player.create("container_1524368774", "", playerParam);
+     });
