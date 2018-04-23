405b5cc7510a256cd2476e65a54fdb34c2d46ca2
gceratic <gceratic@gmail.com>
2018-04-22 19:45:35 -0500

    Add Thumbnail

--- scripts/p1p2.js
+++ scripts/p1p2.js
@@ -1,24 +1,17 @@
 var playerParam = {
   'pcode':'Jvd2wyOrkZVDS3BJWpnvUwEidYIO',
   'playerBrandingId':'f1f3a6490d6f480b91d0d152a863d3c5',
+  'playlistsPlugin': {'data':['5f048115febc49ec9056375198fe2e6e']},
+  'useFirstVideoFromPlaylist': true
   'autoplay':false,
   'loop':false,
   'skin': {
           'config': '../scripts/p1p2.json'
         }
-
 };
+
 
   OO.ready(function() {
 
-  var pp1 = OO.Player.create('container_1524368774', 'V3MDM0ZjE6VFHuScVibWmSCXWU37sTY4', playerPara);
-  OO.player.setEmbedCode("OO.ready(function() {window.pp = OO.Player.create('container_1524428034','QyNDgzZjE6kTGgpMbhcn3rlD77TJ9lpQ', playerParam);});")
-  var pp2 = document.getElementById("pThumbnail"); //player2 being the <div ID> of the "thumbnail".
-  pp2.onclick = play2(); //when the thumbnail is clicked, call pla22() function
-
+  var pp1 = OO.Player.create('container_1524368774', "", playerParam);
    });
-
-  function play2() {
-  pp1.destroy(); //destroying the existing player
-  pp1 = OO.Player.create('container_1524368774', 'QyNDgzZjE6kTGgpMbhcn3rlD77TJ9lpQ', playerParam);
-};
